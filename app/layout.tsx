bcded456b6df6db29025b3c83654dbe05f5e60b6
Thomas GAILLOT <thomas.gaillot@enogrid.com>
2026-02-03 19:27:43 +0100

    app icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,9 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: '32x32' },
-      { url: '/icon.svg', type: 'image/svg+xml' },
+      { url: '/favicon.ico', sizes: '48x48' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
     ],
     apple: '/apple-touch-icon.png',
   },
--- app/manifest.ts
+++ app/manifest.ts
@@ -16,23 +16,18 @@ export default function manifest(): MetadataRoute.Manifest {
         type: 'image/svg+xml',
       },
       {
-        src: '/icon-192.png',
-        sizes: '192x192',
+        src: '/favicon-96x96.png',
+        sizes: '96x96',
         type: 'image/png',
       },
       {
-        src: '/icon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-        src: '/icon-192-maskable.png',
+        src: '/web-app-manifest-192x192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/icon-512-maskable.png',
+        src: '/web-app-manifest-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
